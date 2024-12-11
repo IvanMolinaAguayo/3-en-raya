de9767e414f0ada2144e8e32a4fb34f9babe9600
IvanMolinaAguayo <ivimolinaaguayo@gmail.com>
2024-12-11 20:13:29 +0100

    Importar random y scanner
    Importamos el random y el scanner

---
+++ src/tresEnRaya/tresEnRaya.java
@@ -0,0 +1,12 @@
+package tresEnRaya;
+import java.util.Scanner;
+import java.util.Random;
+public class tresEnRaya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scr = new Scanner(System.in);
+	}
+
+}
