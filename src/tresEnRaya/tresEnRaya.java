3acdfbec2e9f0691716afd81d8ff3bcce4670cd2
IvanMolinaAguayo <ivimolinaaguayo@gmail.com>
2024-12-12 19:16:50 +0100

    Inicio del tres en raya
    Damos la bienvenida a los jugadores y les pedimos su nombre

--- src/tresEnRaya/tresEnRaya.java
+++ src/tresEnRaya/tresEnRaya.java
@@ -7,6 +7,17 @@ public class tresEnRaya {
 		// TODO Auto-generated method stub
 
 		Scanner scr = new Scanner(System.in);
-	}
+		Random random = new Random();
 
+		//Inicio del tres en raya
+		
+		 System.out.println("Bienvenido al juegod de Tres en Raya");
+	        System.out.print("Ingrese el nombre del primer jugador: ");
+	        String jugador1 = scr.nextLine();
+	        System.out.print("Ingrese el nombre del segundo jugador: ");
+	        String jugador2 = scr.nextLine();
+
+	}
 }
+
+
